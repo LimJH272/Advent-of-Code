568ccdf1183c55cf1ac2ba1754cef1c5290b615c
LimJH272 <limjiehan02@gmail.com>
2025-12-04 00:28:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="aoc2024",
+    name="aocLJH",
     version="0.0.1",
     packages=find_packages(),  # will pick up packages with __init__.py
     py_modules=["common_functions"],  # to include the standalone file
